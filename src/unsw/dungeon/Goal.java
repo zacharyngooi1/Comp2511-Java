512584fc3dc1b70ef6a27018dc481b96d8c451f9
z5228846 <z.ngooi@student.unsw.edu.au>
2020-07-19 17:18:37 +1000

    changed to reflect

--- src/unsw/dungeon/Goal.java
+++ src/unsw/dungeon/Goal.java
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-package unsw.dungeon;
-
-public class Goal {
-
-    protected GoalsCompleteInterface implementation;
-	protected boolean isComplete;
-    protected Dungeon dungeon;
-
-    // implements the interfaces check for completion
-    public Goal(Dungeon dungeon, GoalsCompleteInterface implementation) {
-		this.dungeon = dungeon;
-		this.implementation = implementation;
-    }
-
-    public Dungeon getDungeon() {
-		return this.dungeon;
-	}
-    
-    // passes this goal into the the implementation of the interface
-    public boolean checkGoalCompleted() {
-        return this.implementation.isComplete(this);
-    }
-
-    // retunrs current status of this goal
-    public boolean isComplete() {
-        return this.isComplete;
-    }
-
-}
-=======
 package unsw.dungeon;
 
 public interface Goal {
@@ -37,4 +6,3 @@ public interface Goal {
      */
     public boolean isComplete();
 }
->>>>>>> master
